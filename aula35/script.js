1ac15fd3c1832f60cd7fb1eb84d8ccaa3f53b94f
Aliffer Farias <alifferalvesfarias3@gmail.com>
2023-11-09 18:07:16 -0200

    aula sobre parametros

---
+++ aula35/script.js
@@ -0,0 +1,69 @@
+// arguments só serve para funcoes com a palavra function
+function funcao(valor) {
+    console.log(valor, arguments); // arguments é um espaço reservado para todos os argumentos passados para função, independente de terem um parametro ou nao
+}
+funcao('Valor', 1, 2, 3);
+
+console.log("======================")
+
+
+function funcaoSoma() {
+    let total = 0;
+    for (let argumento of arguments) { // um exemplo do que podemos fazer com os arguments mesmo sem ter parametros na nossa função
+        total += argumento
+    }
+    console.log(total)
+
+}
+
+funcaoSoma(1, 2, 3, 4, 5, 6, 7);
+console.log("======================")
+
+
+
+function funcaoSemParametros(a, b, c, d, e) {
+    console.log(a, b, c, d, e) // os valores que nao receberem argumentos ficam setados como undefined
+
+}
+funcaoSemParametros(1, 2, 3, 4)
+console.log("======================")
+
+
+
+function somaNan(a, b = 2, c = 4) {
+    //  b = b || 0 
+    console.log(a + b + c); // pasando somendo um argumento ele soma o numero com undefined e retorna um NaN
+}
+somaNan(2, undefined, 20) // enviando undefined podemos fazer ele assumir o valor padrao de b
+console.log("======================")
+
+function desestruturaçãoObj({ nome, sobrenome, idade }) {
+    console.log(nome, sobrenome, idade);
+}
+let obj = { nome: 'Aliffer', sobrenome: 'Farias', idade: 20 }
+desestruturaçãoObj(obj)
+
+console.log("======================")
+
+function arraysFunc([valor1, valor2, valor3]) {
+    console.log(valor1, valor2, valor3);
+
+}
+
+let meuArray = ['Aliffer', 'Farias', 20];
+arraysFunc(meuArray)
+
+console.log("======================")
+
+const conta = function(operador, acumulador, ...numeros) {// utilizando o rest operator todo o resto depois de acmmulador vai estar em numeros
+    for (let num of numeros) {
+        if (operador === '+') acumulador += num;
+        if (operador === '-') acumulador -= num;
+        if (operador === '*') acumulador *= num;
+        if (operador === '/') acumulador /= num;
+    }
+    console.log(acumulador)
+
+}
+
+conta('*', 1, 20, 30, 40, 50)
