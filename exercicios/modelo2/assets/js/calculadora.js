8ef49223ff2074cf668d179ffb6730a49ab17c85
Aliffer Farias <alifferalvesfarias3@gmail.com>
2023-11-27 14:10:33 -0300

    calculadora

--- exercicios/modelo2/assets/js/calculadora.js
+++ exercicios/modelo2/assets/js/calculadora.js
@@ -1,19 +1,45 @@
 function Calculadora(){
     this.display = document.querySelector('.display');
 
+    this.capturaEnter = () =>{
+        document.addEventListener('keypress', e =>{
+            if(e.key !== 'Enter')return;
+            this.realizaConta();
+        })
+    }
+
     this.capturaCliques = () =>{
         document.addEventListener('click', (e) =>{
             const el = e.target;
             if (el.classList.contains('btn-num')) this.addNumDisplay(el);
+            if (el.classList.contains('btn-clear')) this.clear();
+            if (el.classList.contains('btn-delete')) this.del();
+            if (el.classList.contains('btn-eq')) this.realizaConta(el);
         });
     };
-    this.addNumDisplay = el => this.display.value += el.innerText;
-        
-    
 
-    this.inicia = () =>{
-        this.capturaCliques();
+    this.realizaConta = () =>{
+        try{
+            const conta = eval(this.display.value);
+            if(!conta){
+                alert('Conta inválida')
+            }
+            this.display.value = conta
+        }catch(e){
+            alert('Conta inválida')
+            return
+        }
     };
+    this.inicia = () => {
+        this.capturaEnter();
+        this.capturaCliques();}
+    this.addNumDisplay = el =>{
+        this.display.value += el.innerText;
+        this.display.focus();
+    };
+    this.clear = () => this.display.value = '';
+    this.del = () => this.display.value = this.display.value.slice(0, -1);
+
 
 }
 
