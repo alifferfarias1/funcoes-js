74adcdf5d096b663861d015f3fd18129a8c6f57b
Aliffer Farias <alifferalvesfarias3@gmail.com>
2023-12-14 16:04:15 -0300

    add

---
+++ arrays/array1/script.js
@@ -0,0 +1,16 @@
+// str obj func nums
+// const nomes = new Array('Eduardo', 'Maria', 'Joana') criando um array com constructor
+const nomes = ['eduardo', 'maria', 'joana', 'jordana', 'rosalda']
+const novo = nomes.slice(0, -1);
+
+console.log(nomes)
+console.log(novo)
+
+
+
+const nome = 'Luiz otavio mirando'; // transformando uma string em array usando split separando por espaço
+const nomee = nome.split(' ')
+console.log(nomee)
+
+let nome1 = nomee.join(' ')
+console.log(nome1)
--- aula42/geradoras.js
+++ aula42/geradoras.js
@@ -29,29 +29,40 @@ function* geradora4(){
     yield 6;
 }
 
-const g4 = geradora4();
 
 
 function* geradora5(){
-
+    
     yield function(){
         console.log('vim do y1');
     };
-
+    
+    yield funcaoArrow = () =>{
+        console.log('vim do y2');
+        
+    };
+    
     return function(){
         console.log('vim do return')
     }
-
-    yield funcaoArrow = () =>{
-        console.log('vim do y2');
-
-    };
 }
 
 
+const g2 = geradora2();
+const g4 = geradora4();
 const g5 = geradora5();
+
+console.log(g2.next().value)
+console.log(g2.next().value)
+
+for(let valor of g4){
+    console.log(valor)
+}
+
 const func1 = g5.next().value
 const func2 = g5.next().value
+const func3 = g5.next().value
 
 func1();
 func2();
+func3();
