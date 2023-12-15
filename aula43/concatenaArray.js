43127d07ad0b148a8183c514c9bdd19df1a17a76
Aliffer Farias <alifferalvesfarias3@gmail.com>
2023-12-15 14:46:05 -0300

    funções js

---
+++ aula43/concatenaArray.js
@@ -0,0 +1,8 @@
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+
+const a3 = num1.concat(num2, [7, 8 ,9], 'Meu Array');
+// outra forma const a3 = [...num1 'Luiz', ...num2, ...[7,8,9]];
+console.log(a3);
+
+
---
+++ aula43/splice.js
@@ -0,0 +1,7 @@
+//                -5     -4      -3       -2       -1
+//                 0      1       2        3        4
+const nomes = ['maria', 'joao', 'mario', 'jose', 'joana']
+
+const removidos = nomes.splice(nomes.length, 0, 'Aliffer', 'Otavio');
+
+console.log(nomes, removidos)
