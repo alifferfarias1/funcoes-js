ec7e32e0f254523dbb7792995f589615943f954d
Aliffer Farias <alifferalvesfarias3@gmail.com>
2023-12-24 12:50:01 -0300

    arrays

--- arrays/arrays5/reduce.js
+++ arrays/arrays5/reduce.js
@@ -1,4 +1,6 @@
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+numeros.sort();
+console.log(numeros)
 
 const total = numeros.reduce(function(acumulador, valor, indice, array){
     if (valor % 2 === 0) acumulador += valor;
